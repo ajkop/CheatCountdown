ab920d81a0a1a46c221a1854e21d55ab84e737bc
ajkop <aj.kopczynski@gmail.com>
2019-02-14 19:47:48 -0600

    Added sphynx style doc strings to public methods for future sphynx implementation.
    Modified word_find method to be a little more succinct as well as limit the amount of potential words sent the dictionary lookup method.

--- countdown.py
+++ countdown.py
@@ -18,6 +18,16 @@ class WordScramble:
         self.api_key = oxc.api_key
 
     def word_lookup(self, word, language='en'):
+        """
+        Runs the oxford dictionary API to lookup the word.
+
+        :param word: The word to lookup.
+        :type word: `str`
+        :param language: The language the word is in. Defaults to English ('en')
+        :type language: `str`
+        :return: dict of all the attributes oxford dictionary returns for the word. Or False
+        :rtype: `dict`
+        """
         url_call = self.api_url + f'{language}/' + word.lower()
         headers = {'app_id': self.api_id, 'app_key': self.api_key}
         r = requests.get(url_call, headers=headers)
@@ -31,12 +41,24 @@ class WordScramble:
             return
 
     def get_def(self, word, language='en'):
+        """
+        Get a words definition by connecting to the oxford dictionaries API. Limit 60 per hour.
+
+        :param word: The word to lookup.
+        :type word: `str`
+        :param language: The language the word is in. Defaults to English ('en')
+        :type language: `str`
+        :return: A string containing the definition of the word, or False
+        :rtype: `str`
+        """
+
         lookup_result = self.word_lookup(word, language=language)
         if lookup_result:
             try:
                 lookup_key_map = ['results', 'lexicalEntries', 'entries', 'senses', 'definitions']
                 for key in lookup_key_map:
                     lookup_result = lookup_result[key][0]
+                    continue
                 return lookup_result
             except KeyError:
                 return False
@@ -44,32 +66,62 @@ class WordScramble:
             return False
 
     def word_find(self, letter_list, with_definition=False, limit=10):
+        """
+        Given a list of letters find all real english words that can be found and return a dict of the largest $limit
+        amount. Will also return the dictionary definition of the word if it can find it and the argument is enabled.
+
+        :param letter_list: List of letters to try to make words out of.
+        :type letter_list: `str`
+        :param with_definition: Turn on returning definitions with the word list.
+        :type with_definition: `bool`
+        :param limit: The limit of words to return, defaults to 10
+        :type limit: `int`
+        :return: A dictionary of matched words ordered by their length.
+        :rtype: `dict`
+        """
+
         attempt_list = []
         outcomes = []
         matches = []
 
+        # Iterate over each letter and put them in a list.
         for letter in letter_list:
             attempt_list.append(letter)
+            # For every amount of combinations of letters within range check permutations
             for i in range(1, len(attempt_list)):
                 for item in permutations(attempt_list, i):
                     outcomes.append(item)
 
+        # Join the outcomes together into a new list.
         choices = [''.join(out) for out in outcomes]
 
+        # Iterate over permutations and check if any of them are in our list of words. If so append to matches.
         for choice in choices:
             if str(choice) in self.word_list:
                 matches.append(choice)
 
-        # Strip duplicates from matches.
-        matches = set(list(matches))
+        # Strip duplicates from matches, then Grab the last $limit number of matches after sorting for length
+        limited_matches = sorted(set(list(matches)), key=len, reverse=True)[:limit]
 
+        # If the method is invoked with the with_definition argument do the following dict comprehension.
         if with_definition:
-            return {match: [len(match), self.get_def(match)] for match in sorted(matches, key=len,
-                                                                                 reverse=True)[:limit]}
+            # This comprehension just creates a dictionary of the matched words with word as the key and the len of the
+            # word + definition as the value if a definition is found.
+            return {match: [len(match), self.get_def(match) or 'No definition found'] for match in limited_matches}
         else:
-            return {match: len(match) for match in sorted(matches, key=len, reverse=True)[:limit]}
+            return {match: len(match) for match in limited_matches}
 
     def anagram(self, word, with_definition=False):
+        """
+        Given a word or combination word with no spaces, check for anagrams that match.
+
+        :param word: Word to check anagrams for.
+        :type word: `str`
+        :param with_definition: Optional argument to also return the definition of the word.
+        :type with_definition: `bool`
+        :return: `list' or `dict`
+        """
+
         matches = []
         letter_list = [letter for letter in word]
         outcomes = permutations(letter_list)
--- secret.py
+++ secret.py
@@ -2,5 +2,5 @@ import os
 
 dictionary_file = os.environ.get('DICTIONARY_FILE', None)
 api_url = ' https://od-api.oxforddictionaries.com/api/v1/entries/'
-api_id = '0835d113'
-api_key = '705efabc83f573916060959441c7ea0c'
+api_id = '$API_ID'
+api_key = '$API_KEY'
