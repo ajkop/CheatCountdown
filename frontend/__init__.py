87cb2e5f18ec30d1d4d55187c2743e188aeff95c
ajkop <aj.kopczynski@gmail.com>
2019-04-09 11:12:49 -0500

    Made the project a true package and made my imports not garbage.

--- frontend/__init__.py
+++
@@ -1,5 +0,0 @@
-from .endpoints import app
-
-app.config.from_pyfile('config.py')
-
-app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='countdown',
+    install_requires=['pyenchant', 'requests'],
+    version='1.0',
+    packages=['countdown', 'countdown.frontend', 'countdown.exceptions', 'countdown.api_wrapper'],
+    url='http://countdowncheat.com',
+    license='MIT',
+    author='AJ Kopczynski',
+    author_email='',
+    description='Little flask app to cheat at countdown'
+)
